fdcf9214af77a091bee6cdbcd34f0ac56774c012
Bukola123 <taiwobukola98@@gmail.com>
2021-12-31 17:16:29 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
